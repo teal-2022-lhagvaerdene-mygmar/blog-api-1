2eb5ff29cb366f7f9ea828602d3cb82e58d8228c
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2023-01-31 17:17:38 +0800

    01-31

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const cors = require("cors");
-const { v4: uuid } = require("uuid");
 const fs = require("fs");
-const port = 50000;
+const { v4: uuid } = require("uuid");
 
+const port = 4321;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 function readCategories() {
   const content = fs.readFileSync("categories.json");
   const categories = JSON.parse(content);
   return categories;
 }
+
 app.get("/categories", (req, res) => {
+  const { q } = req.query;
   const categories = readCategories();
-  res.json(categories);
+  console.log(q);
+  if (q) {
+    const filteredList = categories.filter((category) =>
+      category.name.toLowerCase().includes(q.toLowerCase()),
+    );
+
+    res.json(filteredList);
+  } else {
+    res.json(categories);
+  }
 });
 
 app.get("/categories/:id", (req, res) => {
@@ -31,10 +43,27 @@ app.get("/categories/:id", (req, res) => {
 
 app.post("/categories", (req, res) => {
   const { name } = req.body;
-  const categories = readCategories();
   const newCategory = { id: uuid(), name: name };
-  categories.push(newCategory);
+
+  const categories = readCategories();
+
+  categories.unshift(newCategory);
+  fs.writeFileSync("categories.json", JSON.stringify(categories));
+
   res.sendStatus(201);
+});
+
+app.delete("/categories/:id", (req, res) => {
+  const { id } = req.params;
+  const categories = readCategories();
+  const one = categories.find((category) => category.id === id);
+  if (one) {
+    const newList = categories.filter((category) => category.id !== id);
+    fs.writeFileSync("categories.json", JSON.stringify(newList));
+    res.json({ deletedId: id });
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.put("/categories/:id", (req, res) => {
@@ -44,24 +73,13 @@ app.put("/categories/:id", (req, res) => {
   const index = categories.findIndex((category) => category.id === id);
   if (index > -1) {
     categories[index].name = name;
+    fs.writeFileSync("categories.json", JSON.stringify(categories));
     res.json({ updatedId: id });
   } else {
     res.sendStatus(404);
   }
 });
 
-app.delete("/categories/:id", (req, res) => {
-  const { id } = req.params;
-  const categories = readCategories();
-  const one = categories.find((category) => category.id === id);
-  if (one) {
-    const newList = categories.filter((category) => category.id !== id);
-    categories = newList;
-    res.json({ deletedId: id });
-  } else {
-    res.sendStatus(404);
-  }
-});
 app.get("/users/save", (req, res) => {
   const newUser = [
     {
@@ -72,6 +90,7 @@ app.get("/users/save", (req, res) => {
   fs.writeFileSync("data.json", JSON.stringify(newUser));
   res.json(["success"]);
 });
+
 app.get("/users/read", (req, res) => {
   const content = fs.readFileSync("data.json");
   res.json(JSON.parse(content));
@@ -84,6 +103,7 @@ app.get("/users/update", (req, res) => {
   fs.writeFileSync("data.json", JSON.stringify(users));
   res.json({});
 });
+
 app.listen(port, () => {
   console.log("App is listering at port", port);
 });
